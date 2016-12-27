2cd684575a7fc846e1aff51b49fcffaecf773fc2
ajosephmass <Ajith.Joseph2@cognizant.com>
2016-12-27 11:03:27 -0500

    Update SingleTest.java

--- src/test/java/com/browserstack/SingleTest.java
+++ src/test/java/com/browserstack/SingleTest.java
@@ -11,11 +11,12 @@ public class SingleTest extends BrowserStackJUnitTest {
   @Test
   public void test() throws Exception {
     driver.get("https://www.massmutual.com/myaccount/rl/Login/login?type=Customer");
-    WebElement element = driver.findElement(By.name("q"));
+    WebElement element = driver.findElement(By.name("user-name"));
     element.sendKeys("abcdbrowserstack");
-    element.submit();
+    driver.findElement(By.xpath("//div[@id='continue-button']/input")).click();
+    //element.submit();
     Thread.sleep(5000);
 
-    assertEquals("BrowserStack - Google Search", driver.getTitle());
+    //assertEquals("BrowserStack - Google Search", driver.getTitle());
   }
 }
