f3b9e7020eb0f375babab732fa13be843f062d71
sandipjos <sandeep.jose@cognizant.com>
2017-02-19 06:06:07 +0530

    Update SingleTest.java

--- src/test/java/com/browserstack/SingleTest.java
+++ src/test/java/com/browserstack/SingleTest.java
@@ -14,6 +14,10 @@ public class SingleTest extends BrowserStackJUnitTest {
     driver.findElement(By.cssSelector("#lst-ib")).click();
     driver.findElement(By.cssSelector("#lst-ib")).clear();
     driver.findElement(By.cssSelector("#lst-ib")).sendKeys("massmutual");
+    driver.findElement(By.id("searchText")).clear();
+    driver.findElement(By.id("searchText")).sendKeys("test");
+    driver.findElement(By.id("searchSubmit")).click();
+    driver.findElement(By.id("yui_3_10_0_1_1487463576596_198")).click();
     //driver.findElement(By.xpath("//div/div[3]/form/div[2]/div[2]/div[1]/div[1]/div[3]/div/div/div[3]/div/input[1]")).click();
     //driver.findElement(By.xpath("//div/div[3]/form/div[2]/div[2]/div[1]/div[1]/div[3]/div/div/div[3]/div/input[1]")).sendKeys("\n");
     Thread.sleep(5000);
